58ede965a439bf5f7f1cec75fa5f65eaa2e56993
sp42@qq.com <sp42@qq.com>
2024-06-14 08:50:03 +0800

    ci

---
+++ README.md
@@ -0,0 +1,17 @@
+# AJ-Framework 
+
+业务的封装
+
+文档地址：https://framework.ajaxjs.com/docs/aj/
+
+
+# 安装
+要求 Java 1.8+。 Maven 坐标：
+
+```xml
+<dependency>
+  <groupId>com.ajaxjs</groupId>
+  <artifactId>ajaxjs-framework</artifactId>
+  <version>1.0.5</version>
+</dependency>
+```
---
+++ src/main/java/com/ajaxjs/data/jdbc_helper/common/TableName.java
@@ -0,0 +1,20 @@
+package com.ajaxjs.data.jdbc_helper.common;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 表名
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TableName {
+    /**
+     * 表名
+     */
+    String value();
+
+    /**
+     * 创建实体后是否返回新建的 id
+     */
+    boolean isReturnNewlyId() default false;
+}
