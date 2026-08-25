e0b03ab8a0380ac1bbc0d895ac8cec617adbb453
Frank Cheung <frank@ajaxjs.com>
2026-08-25 18:23:17 +0800

    重构

--- aj-spring-base/src/main/java/com/ajaxjs/framework/mvc/unifiedreturn/CustomReturnConverter.java
+++
@@ -1,6 +0,0 @@
-package com.ajaxjs.framework.mvc.unifiedreturn;
-
-import org.springframework.core.convert.converter.Converter;
-
-public abstract class CustomReturnConverter<T> implements Converter<ResponseResultWrapper, T> {
-}
--- aj-spring-base/src/main/java/com/ajaxjs/framework/mvc/unifiedreturn/IUnifiedReturn.java
+++
@@ -1,7 +0,0 @@
-package com.ajaxjs.framework.mvc.unifiedreturn;
-
-/**
- * 自定义返回类型
- */
-public interface IUnifiedReturn {
-}
